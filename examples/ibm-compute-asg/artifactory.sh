e6fbd6ed9df668b9b7e9b0654e018eb2b4e30102
jainish shah <jainishshah@yahoo.com>
2018-08-06 14:16:47 -0700

    no message

--- examples/ibm-compute-asg/artifactory.sh
+++ examples/ibm-compute-asg/artifactory.sh
@@ -1,19 +1,44 @@
 #!/bin/bash -v
+
+export DEBIAN_FRONTEND=noninteractive
+
+# install the wget and curl
+apt-get update
+apt-get -y install wget curl>> /tmp/install-curl.log 2>&1
+
 apt-get update -y
 apt-get install -y software-properties-common curl > /tmp/install.log
 
-# Install Java 8
-add-apt-repository ppa:webupd8team/java -y
-apt-get update -y
+# install Java 8
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
-apt-get install -y oracle-java8-installer > /tmp/install_java8.log
+apt-get install -y oracle-java8-installer>> /tmp/install-java8.log 2>&1
 apt-get install oracle-java8-set-default
+
+#Generate Self-Signed Cert
+mkdir -p /etc/pki/tls/private/ /etc/pki/tls/certs/
+openssl req -nodes -x509 -newkey rsa:4096 -keyout /etc/pki/tls/private/example.key -out /etc/pki/tls/certs/example.pem -days 356 -subj "/C=US/ST=California/L=SantaClara/O=IT/CN=*.localhost"
+
 
 # Install Artifactory
 echo "deb https://jfrog.bintray.com/artifactory-pro-debs xenial main" | tee -a /etc/apt/sources.list
 curl https://bintray.com/user/downloadSubjectPublicKey?username=jfrog | apt-key add -
 apt-get update -y
+apt-get -y install nginx>> /tmp/install-nginx.log 2>&1
 apt-get install -y jfrog-artifactory-pro > /tmp/install_artifactory.log
 
-# Start Artifactory service
+#Install database drivers
+curl -L -o  /opt/jfrog/artifactory/tomcat/lib/mysql-connector-java-5.1.38.jar https://bintray.com/artifact/download/bintray/jcenter/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar
+curl -L -o  /opt/jfrog/artifactory/tomcat/lib/mssql-jdbc-6.2.1.jre8.jar https://bintray.com/artifact/download/bintray/jcenter/com/microsoft/sqlserver/mssql-jdbc/6.2.1.jre8/mssql-jdbc-6.2.1.jre8.jar
+curl -L -o  /opt/jfrog/artifactory/tomcat/lib/postgresql-9.4.1212.jar https://jdbc.postgresql.org/download/postgresql-9.4.1212.jar
+
+echo "artifactory.ping.allowUnauthenticated=true" >> /var/opt/jfrog/artifactory/etc/artifactory.system.properties
+chown artifactory:artifactory -R /var/opt/jfrog/artifactory/*  && chown artifactory:artifactory -R /var/opt/jfrog/artifactory/etc/security && chown artifactory:artifactory -R /var/opt/jfrog/artifactory/etc/*
+
+# start Artifactory
+sleep $((RANDOM % 120))
 service artifactory start
+#service nginx start
+#nginx -s reload
+echo "INFO: Artifactory installation completed."
